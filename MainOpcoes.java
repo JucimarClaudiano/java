d64be8a89b68b5b0ae514ce0b4d118de6aaf5512
root <root@SERVIDOR>
2019-12-02 22:31:18 -0300

    atualizacao

--- MainOpcoes.java
+++ MainOpcoes.java
@@ -15,22 +15,25 @@ public class MainOpcoes
 	//Criando metodo para comunicar com o usuario
 
 	static void interUsuario()
-	{
+	{	
+		//Instaciando a variável opcao, armazenar a opcao do usuário
+		String opcao = "";
+		//Instanciando a classe Scanner
 		Scanner sc = new Scanner(System.in);
 
 
-		//Usando comando de condição
-		
-
-			System.out.println("Digita -0 para sair");
+		//Usando comando de repeticao
+		do{
 
 			int cont = 0;
-			System.out.println("Digite a nota do aluno" + " " + ( cont + 1));
+			System.out.println("Digite a nota do aluno");
 			//Declarando a variavel para receber a nota
 			double notas = sc.nextDouble();
-			System.out.println(notas);
-
-		
+			cont++;
+			System.out.println("Digita s para sair ou c para continuar");
+			opcao = sc.next();
+			System.out.println(opcao);
+		}while(opcao == "s" );
 
 
 	}	
